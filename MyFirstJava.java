12df3a01a4559cc60938af3f0a89021940f1f938
Isuru <isurudara1122@gmail.com>
2022-07-08 13:48:37 +0530

    First commit

---
+++ MyFirstJava.java
@@ -0,0 +1,11 @@
+public class MyFirstJava{
+public static void main(String[] args) {
+
+int count = 0;
+do{
+	System.out.print("Hellow");
+	count+=1;
+}while(count<9);
+	System.out.println();
+	}
+}
